4577d7138fd7cf3ee8b1937e2d562130eeba8d83
NguyenXuanMinh22 <nguyenxuanminh289@gmail.com>
2026-07-03 18:42:04 +0700

    Fix add_to_card

--- page/add_to_cart.php
+++ page/add_to_cart.php
@@ -2,7 +2,7 @@
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
-require_once '../db.php';
+require_once __DIR__ . '/../db.php';
 
 // Kiểm tra đăng nhập
 if (!isset($_SESSION['user_id'])) {
